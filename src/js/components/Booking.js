7f5f42260bd7b04d04e6b9c53350bb4486f6b031
Agnieszka Falandys <agnfal@gmail.com>
2021-06-17 19:53:52 +0200

    add widget base class and two widgets: DatePicker and HourPicker

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,7 @@
 import { templates, select } from '../settings.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 //import { utils } from '../utils.js';
 
 class Booking{
@@ -22,12 +24,18 @@ class Booking{
 
         thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
         thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
+    
+        thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
+        thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
     }
     initWidgets(){
         const thisBooking = this;
         
         thisBooking.peopleAmountWidget = new AmountWidget(thisBooking.dom.peopleAmount); 
         thisBooking.hoursAmountWidget = new AmountWidget(thisBooking.dom.hoursAmount); 
+
+        thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+        thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
     }
 }
 
